0f4c623d58e5c0e4e409cab8b79c061803ff4c8e
Adam O'Neil <adamosoftware@gmail.com>
2024-12-22 11:29:28 -0500

    GameState.IsActive, logging tweaks, resigning

--- Abstractions/GameState.cs
+++ Abstractions/GameState.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Text.Json.Serialization;
 
 namespace Abstractions;
 
@@ -7,25 +8,28 @@ public abstract class GameState<TPlayer, TPiece>(ILogger<GameState<TPlayer, TPie
 	where TPiece : PieceBase
 {
 	protected readonly ILogger<GameState<TPlayer, TPiece>> Logger = logger;
-
+	
 	public abstract int Width { get; init; }
 	public abstract int Height { get; init; }
-	public string CurrentPlayer { get; init; } = default!;
+	public string? CurrentPlayer { get; init; }
+	public bool IsActive { get; init; }
 	public HashSet<TPlayer> Players { get; init; } = [];
+	[JsonIgnore]
 	public Dictionary<string, TPlayer> PlayerNames => Players.ToDictionary(p => p.Name, StringComparer.OrdinalIgnoreCase);
 	public HashSet<TPiece> Pieces { get; init; } = [];
+	[JsonIgnore]
 	public ILookup<string, TPiece> PlayerPieces => Pieces.ToLookup(p => p.PlayerName, StringComparer.OrdinalIgnoreCase);
 
 	public (bool result, string? reason) Validate(TPlayer player, TPiece piece, (int x, int y) location)
 	{
 		if (player.Name != CurrentPlayer) return (false, "Not your turn");
-		if (location.x < 1 || location.x >= Width || location.y < 1 || location.y >= Height) return (false, "Out of bounds");		
+		if (location.x < 1 || location.x >= Width || location.y < 1 || location.y >= Height) return (false, "Out of bounds");
 		return ValidateInner(player, piece, location);
 	}
 
 	protected abstract (bool result, string? reason) ValidateInner(TPlayer player, TPiece piece, (int x, int y) location);
 
-	protected abstract GameState<TPlayer, TPiece> Execute(TPlayer player, TPiece piece, (int x, int y) location);
+	protected abstract (string logTemplate, object?[] logParams, GameState<TPlayer, TPiece>) Execute(TPlayer player, TPiece piece, (int x, int y) location);
 
 	public GameState<TPlayer, TPiece> Play(string playerName, TPiece piece, (int x, int y) location)
 	{
@@ -33,8 +37,20 @@ public abstract class GameState<TPlayer, TPiece>(ILogger<GameState<TPlayer, TPie
 		var (valid, reason) = Validate(player, piece, location);
 		if (!valid) throw new Exception("Invalid move: " + reason);
 
-		Logger.LogInformation("Player {Player} plays {Piece} at {Location}", playerName, piece, location);
+		Logger.LogInformation("{Player} plays {Piece} at {Location}", playerName, piece, location);
 
-		return Execute(player, piece, location);
+		var (template, logParams, result) = Execute(player, piece, location);
+
+		Logger.LogInformation(template, logParams);
+
+		return result;
+	}
+
+	public GameState<TPlayer, TPiece> Resign(string playerName)
+	{
+		var player = PlayerNames[playerName];
+		Logger.LogInformation("{Player} resigns", playerName);
+		player.IsActive = false;
+		return this;
 	}
 }
--- Abstractions/PlayerBase.cs
+++ Abstractions/PlayerBase.cs
@@ -3,8 +3,8 @@
 public class PlayerBase
 {
 	public string Name { get; init; } = default!;
-	public bool IsHuman { get; init; }
-	public bool IsActive { get; init; }
+	public bool IsHuman { get; set; }
+	public bool IsActive { get; set; }
 
 	public override bool Equals(object? obj) => obj is PlayerBase other && Name.Equals(other.Name, StringComparison.OrdinalIgnoreCase);
 
