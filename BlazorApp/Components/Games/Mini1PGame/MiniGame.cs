42cd33829ec0bc7cd085cd93914d0e39a93d2c20
Adam O'Neil <adamosoftware@gmail.com>
2024-12-26 07:09:44 -0500

    getting there

--- BlazorApp/Components/Games/Mini1PGame/MiniGame.cs
+++ BlazorApp/Components/Games/Mini1PGame/MiniGame.cs
@@ -3,8 +3,14 @@ using System.Diagnostics;
 
 namespace BlazorApp.Components.Games.Mini1PGame;
 
+[DebuggerDisplay("{Name}")]
 public class MiniGamePlayer : Player
 {
+	public override string ToString() => Name;
+
+	public override int GetHashCode() => Name.GetHashCode(StringComparison.OrdinalIgnoreCase);
+
+	public override bool Equals(object? obj) => obj is MiniGamePlayer player && Name.Equals(player.Name, StringComparison.OrdinalIgnoreCase);	
 }
 
 [DebuggerDisplay("{Name}")]
---
+++ BlazorApp/PlayArgs.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorApp;
+
+public class PlayArgs
+{
+	public string? LogMessage { get; init; }
+	public object?[] LogParams { get; init; } = [];
+}
