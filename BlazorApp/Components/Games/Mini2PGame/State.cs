ec59cc37c28aa062e2666a8920aede0775b4e13c
Adam O'Neil <adamosoftware@gmail.com>
2024-12-26 12:00:34 -0500

    progress on 2P state

--- BlazorApp/Components/Games/Mini2PGame/State.cs
+++ BlazorApp/Components/Games/Mini2PGame/State.cs
@@ -8,14 +8,27 @@ public class State : GameState<Player, Piece>
 
 	public override uint Height => 30;
 
-	protected override Location[] GetValidMovesInner(Player player, Piece piece)
-	{
-		throw new NotImplementedException();
-	}
+	public const int SpacesPerTurn = 5;
+
+	private int _spacesMoved = 0;
+
+	protected override Location[] GetValidMovesInner(Player player, Piece piece) =>
+		piece.Location
+			.GetAdjacentLocations(Directions.All, SpacesPerTurn - _spacesMoved)
+			.Except(PlayerPieces[player.Name].Select(p => p.Location))
+			.ToArray();
 
 	protected override (string currentPlayer, string? logTemplate, object?[] logParams) PlayInner(Player player, Piece piece, Location location)
 	{
-		throw new NotImplementedException();
+		int distance = piece.Location.Distance(location);
+		_spacesMoved += distance;
+
+		piece.X = location.X;
+		piece.Y = location.Y;
+
+		if (_spacesMoved == SpacesPerTurn) _spacesMoved = 0;
+
+		return (CurrentPlayer!, "{player} moved {piece} {spaces} spaces to {location}", [player, piece, distance, location]);
 	}
 
 	protected override (bool result, string? reason) ValidateInner(Player player, Piece piece, Location location)
