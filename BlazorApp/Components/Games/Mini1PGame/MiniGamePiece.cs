f805a4c68426e4d380973bd1b17a5883230c3db6
Adam O'Neil <adamosoftware@gmail.com>
2024-12-26 07:34:29 -0500

    separating some files

---
+++ BlazorApp/Components/Games/Mini1PGame/MiniGamePiece.cs
@@ -0,0 +1,29 @@
+﻿using Abstractions;
+using System.Diagnostics;
+
+namespace BlazorApp.Components.Games.Mini1PGame;
+
+[DebuggerDisplay("{Name}")]
+public class MiniGamePiece : Piece
+{
+	public required string Name { get; init; }
+
+	public override string ToString() => Name;
+
+	public override int GetHashCode() => Name.GetHashCode(StringComparison.OrdinalIgnoreCase);
+
+	public override bool Equals(object? obj) => obj is MiniGamePiece other && Name.Equals(other.Name, StringComparison.OrdinalIgnoreCase);
+
+	#region maybe-later
+	public static Dictionary<char, string[]> DefaultNames => new()
+	{
+		['a'] = ["Ajax", "Acrid", "Avon", "Anvil"],
+		['b'] = ["Brunswick", "Banjo", "Boom", "Bronze"],
+		['c'] = ["Comet", "Cathode", "Carbon", "Cathode"],
+		['d'] = ["Doodle", "Damsel", "Darius", "Doofus"],
+		['e'] = ["Ember", "Engine", "Euclid", "Enzo"],
+		['f'] = ["Fancy", "Fidelio", "Fulsome", "Fenway"],
+		['g'] = ["Garlic", "Gallium", "Galax", "Galway"]
+	};
+	#endregion
+}
--- BlazorApp/Components/Games/Mini1PGame/MiniGameState.cs
+++ BlazorApp/Components/Games/Mini1PGame/MiniGameState.cs
@@ -1,42 +1,6 @@
 ﻿using Abstractions;
-using System.Diagnostics;
 
 namespace BlazorApp.Components.Games.Mini1PGame;
-
-[DebuggerDisplay("{Name}")]
-public class MiniGamePlayer : Player
-{
-	public override string ToString() => Name;
-
-	public override int GetHashCode() => Name.GetHashCode(StringComparison.OrdinalIgnoreCase);
-
-	public override bool Equals(object? obj) => obj is MiniGamePlayer player && Name.Equals(player.Name, StringComparison.OrdinalIgnoreCase);	
-}
-
-[DebuggerDisplay("{Name}")]
-public class MiniGamePiece : Piece
-{
-	public required string Name { get; init; }
-
-	public override string ToString() => Name;
-
-	public override int GetHashCode() => Name.GetHashCode(StringComparison.OrdinalIgnoreCase);
-
-	public override bool Equals(object? obj) => obj is MiniGamePiece other && Name.Equals(other.Name, StringComparison.OrdinalIgnoreCase);
-
-	#region maybe-later
-	public static Dictionary<char, string[]> DefaultNames => new()
-	{
-		['a'] = ["Ajax", "Acrid", "Avon", "Anvil"],
-		['b'] = ["Brunswick", "Banjo", "Boom", "Bronze"],
-		['c'] = ["Comet", "Cathode", "Carbon", "Cathode"],
-		['d'] = ["Doodle", "Damsel", "Darius", "Doofus"],
-		['e'] = ["Ember", "Engine", "Euclid", "Enzo"],
-		['f'] = ["Fancy", "Fidelio", "Fulsome", "Fenway"],
-		['g'] = ["Garlic", "Gallium", "Galax", "Galway"]
-	};
-	#endregion
-}
 
 /// <summary>
 /// you have 3 pieces, and you can move up to 5 spaces on each turn across your pieces, but not overlapping.
