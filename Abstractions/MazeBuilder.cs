b6cdd9ca1b18adfc6847ec27c465c1c8c5c1387d
Adam O'Neil <adamosoftware@gmail.com>
2025-01-16 06:29:54 -0500

    started on maze builder, but not really doing anything

---
+++ Abstractions/MazeBuilder.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Abstractions;
+
+public class Maze
+{
+	public (uint Width, uint Height) Size { get; init; }
+	public (uint X, uint Y)[][] Routes { get; init; } = [];
+}
+
+public record MazeOptions
+{
+	public (uint Width, uint Height) Size { get; init; }
+	public uint RouteCount { get; init; }
+	public uint MinSegmentLength { get; init; }
+	public (uint Min, uint Max) CorridorWidth { get; init; }	
+}
+
+public static class MazeBuilder
+{
+	public static Maze Create(MazeOptions options) => new()
+	{
+		Size = options.Size,
+		Routes = Enumerable.Range(0, (int)options.RouteCount).Select(_ => CreateRoute(options)).ToArray()
+	};
+
+	private static (uint X, uint Y)[] CreateRoute(MazeOptions options)
+	{
+		var random = new Random();
+		var route = new List<(uint X, uint Y)>();
+		var currentX = (uint)random.Next((int)options.Size.Width);
+		var currentY = (uint)random.Next((int)options.Size.Height);
+
+		for (int i = 0; i < options.MinSegmentLength; i++)
+		{
+			route.Add((currentX, currentY));
+			currentX = (uint)random.Next((int)options.Size.Width);
+			currentY = (uint)random.Next((int)options.Size.Height);
+		}
+
+		return [.. route];
+	}
+}
